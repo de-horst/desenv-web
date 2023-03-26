d925b7cc09c4e5d8a6487771964eb8a522d5162d
de-horst <denisef.horst@gmail.com>
2023-03-26 10:12:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.email.value);
+
+    if (
+      e.target.email.value === "denise.horst@pucpr.br" &&
+      e.target.password.value === "123456"
+    ) {
+      alert("Acessado com sucesso!");
+      e.target.email.value = "";
+      e.target.password.value = "";
+    } else {
+      alert("Usuário ou senha incorretos!");
+    }
+  };
+
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="email">Email </label>
+            <input type="email" name="email" />
+          </div>
+          <div>
+            <label htmlFor="password">Senha </label>
+            <input type="password" name="password" />
+          </div>
+          <button>Acessar</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
